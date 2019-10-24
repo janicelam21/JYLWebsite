89438a5633dd8469b8cd3959170bb3ea7ee453f8
Janice Lam <janice.lam21@gmail.com>
2019-10-24 13:01:17 -0700

    initial setup and file structure setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "import/extensions" : "never",
+    "react/destructuring-assignment": "off",
+    "max-len" : "off",
+    "react/no-array-index-key": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "jsx-a11y/label-has-for": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "global-require": "off",
+    "react/no-access-state-in-setstate": "off",
+  },
+};
